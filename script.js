fff648239c1d1f053b205b94bd961afd0f40673d
Pratham <ppratham812@gmail.com>
2023-03-11 12:55:46 +0530

    Single line joke feature finished

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-let button = document.getElementsByTagName('button')[0];
-let outputDiv = document.querySelector(".container-third");
+let singleButton = document.getElementById('btn-single');
+let multiButton = document.getElementById("btn-two");
+let outputDiv = document.querySelector(".container-output");
 let BaseServerURL = 'https://sv443.net/jokeapi/v2/joke/Any?type=single&format=json';
 
 function SingleLineJokeURL() {
-    return serverURL;
+    return BaseServerURL;
 }
 
 function MultiLineJokeURL() {
     return 'https: //sv443.net/jokeapi/v2/joke/Any?type=twopart&format=json';
 }
+
+// this function will do the work for getting single part joke
+const single = function() {
+    let url = SingleLineJokeURL();
+    fetch(url).
+    then(response => response.json()).
+    then(json => {
+        outputDiv.innerText = json.joke;
+    })
+}
+
+// adding event listeners for two buttons
+singleButton.addEventListener('click', single);
