aad725e83bd224e6ac07275b4d3ab66b20ef6847
Pratham <ppratham812@gmail.com>
2023-03-11 12:45:26 +0530

    WIP: UI created and tested if script is linked or not

---
+++ script.js
@@ -0,0 +1,11 @@
+let button = document.getElementsByTagName('button')[0];
+let outputDiv = document.querySelector(".container-third");
+let BaseServerURL = 'https://sv443.net/jokeapi/v2/joke/Any?type=single&format=json';
+
+function SingleLineJokeURL() {
+    return serverURL;
+}
+
+function MultiLineJokeURL() {
+    return 'https: //sv443.net/jokeapi/v2/joke/Any?type=twopart&format=json';
+}
